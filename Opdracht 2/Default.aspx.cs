d9f358913b27a5b92a027b2da3c8f7d830b5d49b
digitalBlaster55757 <slam01@student.rocvantwente.nl>
2017-03-13 10:22:05 +0100

    Add files via upload

---
+++ Opdracht 2/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void btnMaakKaart_Click(object sender, EventArgs e)
+    {
+        string Naam, Geslacht, Hobby, Opleiding;
+        Naam = txtNaam.Text;
+        Geslacht = rbGeslacht.SelectedValue;
+        Opleiding = cbOpleiding.SelectedValue;
+        Hobby = "hobby: ";
+        if(chkHobby.SelectedValue=="1")
+        {
+            Hobby += "Gamen";
+        }
+        if (chkHobby.SelectedValue=="2")
+        {
+            Hobby += "Programmeren";
+        }
+        Response.Write("Naam: " + Naam + "<br />");
+        Response.Write(Hobby + "<br />");
+        Response.Write("Opleiding: " + Opleiding + "<br />");
+        Response.Write("Geslacht: " + Geslacht + "<br />");
+    }
+protected void RadioButtonList2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (this.RadioButtonList2.SelectedValue == "LFZ")
+            Panel1.Visible = true;
+        else
+            Panel1.Visible = false;
+    }
+   }
