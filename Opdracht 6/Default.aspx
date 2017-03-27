265c46cd725f4a194f9377942a8b57d020102d3b
digitalBlaster55757 <slam01@student.rocvantwente.nl>
2017-03-27 08:49:33 +0200

    Add files via upload

---
+++ Opdracht 6/Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+        <p>
+            De rekenmachine</p>
+        <p>
+            Hieronder staat een grafische rekenmachine.Druk op de knoppen om deze te gebruiken.</p>
+        <p>
+            <asp:TextBox ID="txtScherm" runat="server" Height="61px" Width="270px" Font-Size="X-Large" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        </p>
+        <asp:Button ID="Button7" runat="server" Height="49px" Text="7" Width="52px" Font-Italic="False" Font-Size="Larger" OnClick="Button7_Click" />
+        <asp:Button ID="Button8" runat="server" Height="49px" Text="8" Width="52px" Font-Size="Larger" OnClick="Button8_Click" />
+        <asp:Button ID="Button9" runat="server" Height="49px" Text="9" Width="52px" OnClick="Button9_Click" Font-Size="Larger" />
+        <asp:Button ID="ButtonC" runat="server" Height="49px" Text="C" Width="52px" Font-Size="Larger" OnClick="ButtonC_Click" />
+        <asp:Button ID="ButtonBack" runat="server" Height="49px" Text="Back" Width="52px" Font-Size="Large" OnClick="ButtonBack_Click" />
+        <p>
+            <asp:Button ID="Button4" runat="server" Height="49px" Text="4" Width="52px" Font-Size="Larger" OnClick="Button4_Click" />
+            <asp:Button ID="Button5" runat="server" Height="49px" Text="5" Width="52px" Font-Size="Larger" OnClick="Button5_Click" />
+            <asp:Button ID="Button6" runat="server" Height="49px" Text="6" Width="52px" Font-Size="Larger" OnClick="Button6_Click" />
+            <asp:Button ID="ButtonPlus" runat="server" Height="49px" Text="+" Width="52px" Font-Size="Larger" OnClick="ButtonPlus_Click" />
+            <asp:Button ID="ButtonM2" runat="server" Height="49px" Text="M2" Width="52px" Font-Size="Larger" />
+        </p>
+        <asp:Button ID="Button1" runat="server" Height="49px" Text="1" Width="52px" Font-Size="Larger" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" Height="49px" Text="2" Width="52px" Font-Size="Larger" OnClick="Button2_Click" />
+        <asp:Button ID="Button3" runat="server" Height="49px" Text="3" Width="52px" Font-Size="Larger" OnClick="Button3_Click" />
+        <asp:Button ID="ButtonMin" runat="server" Height="49px" Text="-" Width="52px" Font-Size="Larger" OnClick="ButtonMin_Click" />
+        <asp:Button ID="ButtonO1" runat="server" Height="49px" Text="O1" Width="52px" Font-Size="Larger" />
+        <p>
+            <asp:Button ID="Button0" runat="server" Height="49px" Text="0" Width="110px" Font-Size="Larger" OnClick="Button0_Click" />
+            <asp:Button ID="ButtonPunt" runat="server" Height="49px" Text="." Width="52px" Font-Size="Larger" OnClick="ButtonPunt_Click" />
+            <asp:Button ID="ButtonIs" runat="server" Height="49px" Text="=" Width="52px" Font-Size="Larger" OnClick="ButtonIs_Click" />
+            <asp:Button ID="ButtonO2" runat="server" Height="49px" Text="O2" Width="52px" Font-Size="Larger" />
+        </p>
+        <p>
+            <asp:Label ID="lblGetal1" runat="server" Text="Getal 1" Visible="False"></asp:Label>
+            <asp:Label ID="lblBereken" runat="server" Text="Berekenen" Visible="False"></asp:Label>
+        </p>
+    
+    </form>
+</body>
+</html>
---
+++ Opdracht 6/Default.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "9";
+        }
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "1";
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "2";
+        }
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "3";
+        }
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "4";
+        }
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "5";
+        }
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "6";
+        }
+    }
+
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "7";
+        }
+    }
+
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "8";
+        }
+    }
+
+    protected void ButtonPunt_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Contains(",") != true)
+        {
+            if (txtScherm.Text.Length == 0)
+            {
+                txtScherm.Text += "0";
+            }
+            else
+            {
+                txtScherm.Text += ",";
+            }
+        }
+
+    }
+
+    protected void Button0_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length == 0)
+        {
+            txtScherm.Text += "";
+        }
+        else
+        {
+            txtScherm.Text += "0";
+        }
+    }
+
+    protected void ButtonPlus_Click(object sender, EventArgs e)
+    {
+        lblGetal1.Text = txtScherm.Text;
+        txtScherm.Text = string.Empty;
+        lblBereken.Text = "+";
+    }
+
+    protected void ButtonIs_Click(object sender, EventArgs e)
+    {
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double G2 = Convert.ToDouble(lblGetal1.Text);
+        double Uitkomst = 0;
+
+        if (lblBereken.Text == "+")
+        {
+            Uitkomst = G1 + G2;
+        }
+        else if(lblBereken.Text == "-")
+        {
+            Uitkomst = G1 - G2;
+        }
+        txtScherm.Text = Uitkomst.ToString();
+       
+    }
+
+    protected void ButtonC_Click(object sender, EventArgs e)
+    {
+        txtScherm.Text = "";
+    }
+
+    protected void ButtonBack_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length > 0)
+        {
+            txtScherm.Text = txtScherm.Text.Remove(txtScherm.Text.Length - 1);
+        }
+    }
+
+    protected void ButtonMin_Click(object sender, EventArgs e)
+    {
+        {
+            lblGetal1.Text = txtScherm.Text;
+            txtScherm.Text = string.Empty;
+            lblBereken.Text = "-";
+        }
+    }
+}
